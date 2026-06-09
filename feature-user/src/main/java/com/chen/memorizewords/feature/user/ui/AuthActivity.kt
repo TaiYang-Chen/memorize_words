e8fed316264e5ed15e61b9b4ca002be235823a9c
chen <1563011912@qq.com>
2026-06-09 20:34:52 +0800

    456456456754

--- feature-user/src/main/java/com/chen/memorizewords/feature/user/ui/AuthActivity.kt
+++ feature-user/src/main/java/com/chen/memorizewords/feature/user/ui/AuthActivity.kt
@@ -3,15 +3,19 @@ package com.chen.memorizewords.feature.user.ui
 import android.content.Intent
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.get
+import com.chen.memorizewords.core.navigation.AuthEntry
+import com.chen.memorizewords.core.navigation.AuthEntryDestination
 import com.chen.memorizewords.core.navigation.HomeEntry
 import com.chen.memorizewords.core.navigation.OnboardingEntry
 import com.chen.memorizewords.core.ui.activity.BaseVmDbActivity
 import com.chen.memorizewords.core.ui.vm.BaseViewModel
 import com.chen.memorizewords.domain.account.auth.AuthStateProvider
 import com.chen.memorizewords.domain.account.repository.LocalAccountRepository
+import com.chen.memorizewords.feature.user.R
 import com.chen.memorizewords.feature.user.auth.social.QQAuthProvider
 import com.chen.memorizewords.feature.user.databinding.ModuleUserActivityAuthBinding
-import com.chen.memorizewords.core.navigation.AuthEntry
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -49,6 +53,35 @@ class AuthActivity : BaseVmDbActivity<BaseViewModel, ModuleUserActivityAuthBindi
     }
 
     override fun initView(savedInstanceState: Bundle?) {
+        if (savedInstanceState == null) {
+            setupAuthGraph()
+        }
+    }
+
+    private fun setupAuthGraph() {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val graph = navController.navInflater.inflate(R.navigation.nav_module_auth)
+        graph.setStartDestination(
+            if (isUserProfileEntry()) {
+                graph[R.id.module_login_profilefragment].id
+            } else {
+                graph[R.id.loginFragment].id
+            }
+        )
+        navController.graph = graph
+    }
+
+    private fun isUserProfileEntry(): Boolean {
+        val data = intent?.data ?: return false
+        val normalizedUri = buildString {
+            append(data.scheme)
+            append("://")
+            append(data.host)
+            append(data.path.orEmpty())
+        }
+        return normalizedUri == AuthEntryDestination.USER_PROFILE_DEEP_LINK
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
